f9f83719aa499d588e4f39e80e22b5f2f0c0f759
Aleksandr Galiuzov <AGaliuzov@luxoft.com>
2014-11-04 17:23:00 +0200

    Made proper initialization of parameters list

--- src/components/application_manager/src/application_manager_impl.cc
+++ src/components/application_manager/src/application_manager_impl.cc
@@ -1194,7 +1194,10 @@ void ApplicationManagerImpl::SendMessageToMobile(
       smart_objects::SmartMap::iterator iter_end = s_map.map_end();
 
       for (; iter != iter_end; ++iter) {
-        params.push_back(iter->first);
+        if (true == iter->second.asBool()) {
+          LOG4CXX_INFO(logger_, "Request's param: " << iter->first);
+          params.push_back(iter->first);
+        }
       }
     }
     const mobile_apis::Result::eType check_result =
