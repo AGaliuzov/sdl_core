4d17c6f97d32c4ad519dc3584aff95dc26cb27ed
Elisey Zamakhov <EZamakhov@luxoft.com>
2014-11-16 16:27:45 +0400

    Fix logger deinitilization and assert usage in release mode Add NDEBUG/DEBUG flag to main cmake build and RTC build Update genivi cmake
    Conflicts:
    	src/components/transport_manager/src/usb/libusb/usb_handler.cc

--- src/components/transport_manager/src/usb/libusb/usb_handler.cc
+++ src/components/transport_manager/src/usb/libusb/usb_handler.cc
@@ -39,6 +39,7 @@
 #include "transport_manager/usb/common.h"
 #include "transport_manager/transport_adapter/transport_adapter_impl.h"
 
+#include "utils/macro.h"
 #include "utils/logger.h"
 
 namespace transport_manager {
@@ -357,13 +358,13 @@ void UsbHandler::SubmitControlTransfer(
   assert(transfer->Type() == UsbControlTransfer::VENDOR);
   const libusb_request_type request_type = LIBUSB_REQUEST_TYPE_VENDOR;
 
-  libusb_endpoint_direction endpoint_direction;
+  libusb_endpoint_direction endpoint_direction = LIBUSB_ENDPOINT_IN;
   if (transfer->Direction() == UsbControlTransfer::IN) {
     endpoint_direction = LIBUSB_ENDPOINT_IN;
   } else if (transfer->Direction() == UsbControlTransfer::OUT) {
     endpoint_direction = LIBUSB_ENDPOINT_OUT;
   } else {
-    assert(0);
+    NOTREACHED();
   }
   const uint8_t request = transfer->Request();
   const uint16_t value = transfer->Value();
