3e7988b9d8d664f3e12a8e6be507260dd35ff374
Le Hang <LTTHang@luxoft.com>
2015-01-15 15:35:21 +0700

    revert APPLINK-10625 not a subject for review

--- skip_qnx_lcns.sh
+++ skip_qnx_lcns.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-sed -i "s/CC_cfg_3rd_party = \${QNX_HOST}\/usr\/bin\/qcc/CC_cfg_3rd_party = \${QNX_HOST}\/usr\/bin\/ntoarmv7-gcc/" cfg/qnx_armv7.mk
-sed -i "s/CPP_cfg_3rd_party = \${QNX_HOST}\/usr\/bin\/qcc/CPP_cfg_3rd_party = \${QNX_HOST}\/usr\/bin\/ntoarmv7-g++/" cfg/qnx_armv7.mk
 sed -i "s/CC = \${QNX_HOST}\/usr\/bin\/qcc/CC = \${QNX_HOST}\/usr\/bin\/ntoarmv7-gcc/" cfg/qnx_armv7.mk
 sed -i "s/CPP = \${QNX_HOST}\/usr\/bin\/qcc/CPP = \${QNX_HOST}\/usr\/bin\/ntoarmv7-g++/" cfg/qnx_armv7.mk
 sed -i "s/LD = qcc/LD = \${QNX_HOST}\/usr\/bin\/ntoarmv7-g++/" cfg/qnx_armv7.mk
