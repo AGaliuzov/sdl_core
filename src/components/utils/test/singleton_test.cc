245e5701222f3744262769a2d3e94edf4021cbcf
Veronica Veremjova <VVeremjova@luxoft.com>
2015-01-27 15:20:06 +0200

    APPLINK-11062 not a subject for review, changed names

--- src/components/utils/test/singleton_test.cc
+++ src/components/utils/test/singleton_test.cc
@@ -88,7 +88,6 @@ TEST(SingletonTest, DestroySingletonTwice) {
 
 TEST(SingletonTest, DeleteSingletonCreateAnother) {
   //arrange
-  SingletonTest::instance();
   SingletonTest::instance()->SetValue(10);
   //assert
   ASSERT_TRUE(SingletonTest::exists());
@@ -108,8 +107,8 @@ TEST(SingletonTest, DeleteSingletonCreateAnother) {
 }
 
 void* func_pthread1(void*) {
-  SingletonTest::instance();
-  pthread_exit(SingletonTest::instance());
+  SingletonTest* singletone_in_other_thread =  SingletonTest::instance();
+  pthread_exit(singletone_in_other_thread);
   return NULL;
 }
 
