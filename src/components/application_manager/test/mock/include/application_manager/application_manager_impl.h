fb3b4c0945c833bbeec56434de6abffadead761e
Aleksandr Galiuzov <AGaliuzov@luxoft.com>
2015-02-17 17:10:48 +0200

    APPLINK-8556 Add changes to fix tests building

--- src/components/application_manager/test/mock/include/application_manager/application_manager_impl.h
+++ src/components/application_manager/test/mock/include/application_manager/application_manager_impl.h
@@ -271,6 +271,9 @@ class ApplicationManagerImpl : public ApplicationManager,
   MOCK_METHOD0(OnTimerSendTTSGlobalProperties, void());
   MOCK_METHOD0(CreatePhoneCallAppList, void());
   MOCK_METHOD0(ResetPhoneCallAppList, void());
+  MOCK_METHOD2(ChangeAppsHMILevel, void(uint32_t, mobile_apis::HMILevel::eType));
+  MOCK_METHOD1(MakeAppNotAudible, void(uint32_t app_id));
+  MOCK_METHOD1(MakeAppFullScreen, bool(uint32_t app_id));
   MOCK_METHOD1(AddAppToTTSGlobalPropertiesList, void(const uint32_t));
   MOCK_METHOD1(RemoveAppFromTTSGlobalPropertiesList, void(const uint32_t));
   MOCK_METHOD1(application_by_hmi_app, ApplicationSharedPtr(uint32_t));
