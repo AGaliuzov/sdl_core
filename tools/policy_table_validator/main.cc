e2c92423588a7d0da8b7d17b8a754ec28bfdddef
vveremeva <vveremjova@luxoft.com>
2015-03-17 10:39:28 +0200

    APPLINK-11550 not a subject for review  Reason: ford headers

--- tools/policy_table_validator/main.cc
+++ tools/policy_table_validator/main.cc
@@ -1,3 +1,35 @@
+/*
+ * Copyright (c) 2015, Ford Motor Company
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * Neither the name of the Ford Motor Company nor the names of its contributors
+ * may be used to endorse or promote products derived from this software
+ * without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <iostream>
 #include <cstdlib>
 #ifdef EXTENDED_POLICY
@@ -19,7 +51,8 @@ enum ResultCode {
   PT_TYPE_ERROR
 };
 
-rpc::policy_table_interface_base::PolicyTableType StringToPolicyTableType(const std::string& str_pt_type) {
+rpc::policy_table_interface_base::PolicyTableType StringToPolicyTableType(
+    const std::string& str_pt_type) {
   if (str_pt_type == "PT_PRELOADED") {
     return rpc::policy_table_interface_base::PT_PRELOADED;
   }
@@ -33,10 +66,10 @@ rpc::policy_table_interface_base::PolicyTableType StringToPolicyTableType(const 
 }
 
 void help() {
-  std::cout << "Usage:" << std::endl <<
-               "./policy_validator {Policy table type} {file_name}" << std::endl;
+  std::cout << "Usage:" << std::endl
+      << "./policy_validator {Policy table type} {file_name}" << std::endl;
   std::cout << "Policy table types:"
-               "\t PT_PRELOADED , PT_UPDATE , PT_SNAPSHOT" <<std::endl;
+      "\t PT_PRELOADED , PT_UPDATE , PT_SNAPSHOT" << std::endl;
 }
 
 int main(int argc, char** argv) {
@@ -48,8 +81,8 @@ int main(int argc, char** argv) {
   std::string pt_type_str = argv[1];
   std::string file_name = argv[2];
   std::string json_string;
-  rpc::policy_table_interface_base::PolicyTableType pt_type;
-  pt_type = StringToPolicyTableType(pt_type_str);
+  rpc::policy_table_interface_base::PolicyTableType pt_type =
+      StringToPolicyTableType(pt_type_str);
   if (rpc::policy_table_interface_base::PolicyTableType::INVALID_PT_TYPE
       == pt_type) {
     std::cout << "Invalid policy table type: " << pt_type_str << std::endl;
@@ -77,7 +110,7 @@ int main(int argc, char** argv) {
   policy_table::Table table(&value);
   table.SetPolicyTableType(pt_type);
   bool is_valid = table.is_valid();
-  if (true == is_valid ) {
+  if (true == is_valid) {
     std::cout << "Table is valid" << std::endl;
     exit(SUCCES);
   }
@@ -85,7 +118,8 @@ int main(int argc, char** argv) {
   std::cout << "Table is not valid" << std::endl;
   rpc::ValidationReport report("policy_table");
   table.ReportErrors(&report);
-  std::cout << "Errors: " << std::endl << rpc::PrettyFormat(report) << std::endl;
+  std::cout << "Errors: " << std::endl << rpc::PrettyFormat(report)
+      << std::endl;
 
   return SUCCES;
 }
