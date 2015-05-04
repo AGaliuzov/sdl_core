63c93c00b7f10109df3529e2c117cae3830cc2ad
Dmitriy Klimenko <dklimenko@luxoft.com>
2015-05-04 13:45:30 -0700

    APPLINK-12026: UI.GetCapabilities: change HMI_API for SoftButtonCapabilities to be an array

--- src/components/application_manager/src/commands/mobile/register_app_interface_request.cc
+++ src/components/application_manager/src/commands/mobile/register_app_interface_request.cc
@@ -410,10 +410,7 @@ void RegisterAppInterfaceRequest::SendRegisterAppInterfaceResponseToMobile(
       *hmi_capabilities.button_capabilities();
   }
   if (hmi_capabilities.soft_button_capabilities()) {
-    /* HMI softButtonCapabilities has type object,
-       but in Mobile API it is vector
-    */
-    response_params[hmi_response::soft_button_capabilities][0] =
+    response_params[hmi_response::soft_button_capabilities] =
       *hmi_capabilities.soft_button_capabilities();
   }
   if (hmi_capabilities.preset_bank_capabilities()) {
