a84ff743cefdddc2ef75dbc8f302e590cc9ff267
Alexander Kutsan <AKutsan@luxoft.com>
2015-06-08 12:51:25 +0300

    APPLINK-13465 Make workaround for google bug

--- src/components/utils/test/scope_guard_test.cc
+++ src/components/utils/test/scope_guard_test.cc
@@ -38,19 +38,19 @@ namespace test {
 namespace components {
 namespace utils {
 
-  using ::utils::ScopeGuard;
-  using ::utils::MakeGuard;
-  using ::utils::MakeObjGuard;
+using ::utils::ScopeGuard;
+using ::utils::MakeGuard;
+using ::utils::MakeObjGuard;
 
 class TestObject {
-  public:
-    MOCK_METHOD0(function_to_call, void ());
-    MOCK_METHOD1(function_to_call_with_param, void (void*));
+ public:
+  MOCK_METHOD0(function_to_call, void ());
+  MOCK_METHOD1(function_to_call_with_param, void (void*));
 };
 
 namespace {
-  static int call_with_param_count;
-  void dealloc(char* ptr) { delete ptr; ++call_with_param_count;}
+static int call_with_param_count;
+void dealloc(char* ptr) { delete ptr; ++call_with_param_count;}
 }
 
 TEST(ScopeGuardTest, CallFreeFunctionWithParam) {
@@ -64,12 +64,14 @@ TEST(ScopeGuardTest, CallFreeFunctionWithParam) {
 }
 
 TEST(ScopeGuardTest, CallObjectFunction) {
-    TestObject obj;
-    EXPECT_CALL(obj, function_to_call()).Times(1);
-    {
-      ScopeGuard guard = MakeObjGuard(obj, &TestObject::function_to_call);
-      UNUSED(guard);
-    }
+  TestObject obj;
+  using ::testing::Mock;
+  Mock::AllowLeak(&obj); // Google tests bug
+  EXPECT_CALL(obj, function_to_call()).Times(1);
+  {
+    ScopeGuard guard = MakeObjGuard(obj, &TestObject::function_to_call);
+    UNUSED(guard);
+  }
 }
 
 TEST(ScopeGuardTest, CallObjectFunctionWithParam) {
