5e9337b90f9a2c4f6475d72a4b8d09efcf1f3288
Andriy Kozoriz <AKozoriz@luxoft.com>
2015-11-17 16:06:52 +0200

    APPLINK-18976_Move_interfaces_to_include_folder
    	Reason: 1) connection_handler :
                            1.1) connection_handler global interfaces moved to:
                            ./src/components/include/connection_handler/
                            1.2) connection_handler mocks moved to :
                            ./src/components/include/test/connection_handler
                            1.3) All depend cmake files changed
                    2) hmi_message_handler :
                            2.1) hmi_message_handler global interfaces moved to:
                            ./src/components/include/hmi_message_handler/
                            2.2) hmi_message_handler mocks moved to :
                            ./src/components/include/test/hmi_message_handler
                            2.3) All depend cmake files changed
    		3) Deleted unused mock_subscriber.h (duplicate)

--- src/components/connection_handler/test/connection_handler_impl_test.cc
+++ src/components/connection_handler/test/connection_handler_impl_test.cc
@@ -39,7 +39,7 @@
 // TODO(EZamakhov): move security test
 #include "security_manager_mock.h"
 #include "protocol_handler_mock.h"
-#include "connection_handler_observer_mock.h"
+#include "connection_handler/connection_handler_observer_mock.h"
 #include "transport_manager_mock.h"
 
 namespace test {
