56521d179928b6edddf57b6a223b382497cbcb03
Andriy Kozoriz <AKozoriz@luxoft.com>
2016-01-14 11:58:45 +0200

    Remove useless chech from DialNumberRequest::on_event
    	Removed useless check for empty string, because
    		this state will checked in SendResponse method.
    
    	Related to : 13202

--- src/components/application_manager/src/commands/mobile/dial_number_request.cc
+++ src/components/application_manager/src/commands/mobile/dial_number_request.cc
@@ -121,8 +121,7 @@ void DialNumberRequest::on_event(const event_engine::Event& event) {
 
   const bool is_success = mobile_apis::Result::SUCCESS == result_code;
   const bool is_info_valid =
-      message[strings::msg_params].keyExists(strings::info) &&
-      !message[strings::msg_params][strings::info].empty();
+      message[strings::msg_params].keyExists(strings::info);
 
   if (is_info_valid) {
     const char* info_char_array =
